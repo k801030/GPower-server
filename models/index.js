d9063170c583cbc0e11b7f88bdcac70e6789288f
Even <kiki44552002@gmail.com>
2015-06-12 16:57:50 +0800

    refactor

--- models/index.js
+++ models/index.js
@@ -3,18 +3,29 @@ mongoose.connect("mongodb://localhost/chirpy");
 var Schema = mongoose.Schema;
 var chirpy = new Schema({
   name: String,
-  satistification: { type: Number, min: 0, max: 10 },
-  vitamin: { type: Number, min: 0, max: 100 },
-  step: { type: Number }
+  satistification: {
+    type: Number,
+    min: 0,
+    max: 10
+  },
+  vitamin: {
+    type: Number,
+    min: 0,
+    max: 100
+  },
+  step: {
+    type: Number
+  }
 
 });
+
 chirpy.methods.feed = function() {
   this.vitamin -= 20;
-  this.satistification += parseInt(Math.random()*3+1);
-}
-chirpy.methods.walk = function(step) { 
-  this.vitamin += step/5;
-}
+  this.satistification += parseInt(Math.random() * 3 + 1);
+};
+chirpy.methods.walk = function(step) {
+  this.vitamin += step / 5;
+};
 
 var Chirpy = mongoose.model('Chirpy', chirpy);
 
--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,70 @@
 var express = require('express');
 var router = express.Router();
-var chirpy = require('../models/');
+var Chirpy = require('../models/');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
+
+// create
 router.post('/chirpy', function(req, res, next) {
-  var chirpy = new Chirpy({ name: req.body.name });
+  var chirpy = new Chirpy({
+    name: req.body.name
+  });
+
+  chirpy.save(function() {
+    res.json({
+      status: 'success'
+    });
+  });
+
 });
 
 router.get('/chirpies', function(req, res, next) {
-  Chirpy.find(
-    {},
-    function(err, chirpies){
+  Chirpy.find({},
+    function(err, chirpies) {
       res.json(chirpies);
-  });
-})
+    });
+});
 
 
 router.get('/chirpy/:id', function(req, res, next) {
-  Chirpy.update(
-    {_id: req.param.id },
-    function(err, chirpy){
+  console.log(req.params.id);
+  Chirpy.find({
+      _id: req.params.id
+    },
+    function(err, chirpy) {
       res.json(chirpy);
-  });
-})
+    });
+});
 
 
-router.post('chirpy/:id/feed', function(req, res, next) {
-  Chirpy.find(
-    {_id: req.param.id },
-    function(err, chirpy){
+router.post('/chirpy/:id/feed', function(req, res, next) {
+  Chirpy.find({
+      _id: req.params.id
+    },
+    function(err, chirpy) {
       chirpy.feed();
       res.josn(chirpy);
     }
   );
-})
+});
 
-router.post('chirpy/:id/walk', function(req, res, next) {
-  Chirpy.find(
-    {_id: req.param.id },
-    function(err, chirpy){
+router.post('/chirpy/:id/walk', function(req, res, next) {
+  Chirpy.find({
+      _id: req.params.id
+    },
+    function(err, chirpy) {
       chirpy.walk();
       res.json(chirpy);
     }
   );
-})
+});
 
 
 module.exports = router;
